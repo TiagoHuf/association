faf59f783729d505d01cfd656f847ad1d53851f9
Tiago Huf <tiago.hen.99@gmail.com>
2024-03-08 22:07:20 -0300

    feat: Implementado seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,36 +1,39 @@
-# db/seeds.rb
 require 'faker'
 require 'cpf_cnpj'
 
-# Crie um usuário de exemplo
+Debt.destroy_all
+Person.destroy_all
+User.destroy_all
+
 User.create(
-  email: 'admin@example.com',
-  encrypted_password: 'password' # É altamente recomendável não usar senhas em texto plano em um ambiente de produção
+  email: 'admin@admin.com',
+  password: '1234567'
 )
 
-# Crie mais alguns usuários aleatórios e armazene seus IDs
 user_ids = []
 50.times do
   user = User.create(
     email: Faker::Internet.email,
-    encrypted_password: Faker::Internet.password
+    password: Faker::Internet.password
   )
   user_ids << user.id
 end
 
+person_ids = []
 100.times do
-  Person.create(
+  person = Person.create(
     name: Faker::Name.name,
     phone_number: Faker::PhoneNumber.phone_number,
     national_id: CPF.generate,
     active: Faker::Boolean.boolean,
-    user_id: user_ids.sample # Seleciona aleatoriamente um dos IDs armazenados
+    user_id: user_ids.sample
   )
+  person_ids << person.id
 end
 
 500.times do
   Debt.create(
-    person_id: Faker::Number.between(from: 1, to: 100), # Supondo que existem 100 pessoas na tabela people
+    person_id: person_ids.sample,
     amount: Faker::Number.decimal(l_digits: 2),
     observation: Faker::Lorem.sentence
   )
