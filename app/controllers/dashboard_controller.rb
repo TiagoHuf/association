a4a499859b0d8a7988459e22fba73165cab059d9
Tiago Huf <tiago.hen.99@gmail.com>
2024-04-19 21:24:31 -0300

    feat: Implementado dashboard

---
+++ app/controllers/dashboard_controller.rb
@@ -0,0 +1,43 @@
+class DashboardController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @dashboard_service = DashboardService.new(current_user)
+  end
+
+  def active_people_pie_chart
+    @dashboard_service.active_people_pie_chart
+  end
+
+  def total_debts
+    @dashboard_service.total_debts
+  end
+
+  def total_payments
+    @dashboard_service.total_payments
+  end
+
+  def amount
+    @dashboard_service.amount
+  end
+
+  def last_debts
+    @dashboard_service.last_debts
+  end
+
+  def last_payments
+    @dashboard_service.last_payments
+  end
+
+  def my_people
+    @dashboard_service.my_people
+  end
+
+  def top_person
+    @dashboard_service.top_person
+  end
+
+  def bottom_person
+    @dashboard_service.bottom_person
+  end
+end
